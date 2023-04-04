6138662e3a5cfba722ef1abe214743b2cd36a97e
Eduardo Hoffmann <eduhoffmann.sistemas@yahoo.com.br>
2023-04-04 15:22:02 -0300

    Interface inicial

---
+++ Instagram/Controllers/FeedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FeedViewController.swift
+//  Instagram
+//
+//  Created by Eduardo Hoffmann on 04/04/23.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
